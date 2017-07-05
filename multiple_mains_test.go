4a643a04f2147976acb8990cb31b439ec113b90c
Uriel Corfa <korfuri@gmail.com>
2017-07-05 23:30:46 +0800

    Add a test that verifies that multiple mains can be loaded.

---
+++ multiple_mains_test.go
@@ -0,0 +1,27 @@
+package goref
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+
+func TestMultipleMains(t *testing.T) {
+	const (
+		pkgbase = "github.com/korfuri/goref/testdata/multiple_mains/"
+		filebase = "testdata/multiple_mains/"
+	)
+
+	pg := NewPackageGraph()
+	pg.LoadProgram(pkgbase + "1/main", filebase + "1/main.go")
+	assert.Contains(t, pg.Packages, pkgbase + "1/main")
+	assert.Contains(t, pg.Packages, pkgbase + "common")
+	common := pg.Packages[pkgbase + "common"]
+	assert.Len(t, pg.Packages, 2)
+	pg.LoadProgram(pkgbase + "2/main", filebase + "2/main.go")
+	assert.Contains(t, pg.Packages, pkgbase + "1/main")
+	assert.Contains(t, pg.Packages, pkgbase + "2/main")
+	assert.Contains(t, pg.Packages, pkgbase + "common")
+	assert.Len(t, pg.Packages, 3)
+	assert.Equal(t, common, pg.Packages[pkgbase + "common"])
+}
---
+++ testdata/multiple_mains/2/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/korfuri/goref/testdata/multiple_mains/common"
+
+func main() {
+	common.Fun()
+}
