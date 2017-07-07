f97a196a40767fc931ecab2c9e9486674a8177d8
Uriel Corfa <korfuri@gmail.com>
2017-07-07 22:19:39 +0800

    Add a test for candidatePaths

--- loadpath_test.go
+++ loadpath_test.go
@@ -10,3 +10,13 @@ func TestCleanImportSpec(t *testing.T) {
 	assert.Equal(t, "foo/bar/baz", cleanImportSpec(&ast.ImportSpec{Path: &ast.BasicLit{Value: "foo/bar/baz"}}))
 	assert.Equal(t, "foo/bar/baz", cleanImportSpec(&ast.ImportSpec{Path: &ast.BasicLit{Value: "\"foo/bar/baz\""}}))
 }
+
+func TestCandidatePaths(t *testing.T) {
+	r := []string{
+		"a/b/vendor/c/d",
+		"a/vendor/c/d",
+		"vendor/c/d",
+		"c/d",
+	}
+	assert.Equal(t, r, candidatePaths("c/d", "a/b"))
+}
