e504cdab83c2b52e24c26a500901a720176ee564
Uriel Corfa <korfuri@gmail.com>
2017-07-09 21:21:20 +0800

    Make Ref serializable in JSON

--- ref.go
+++ ref.go
@@ -1,6 +1,7 @@
 package goref
 
 import (
+	"encoding/json"
 	"fmt"
 )
 
@@ -37,4 +38,41 @@ func (r *Ref) String() string {
 		r.RefType,
 		r.ToPackage, r.ToIdent, r.ToPosition,
 		r.FromPackage, r.FromIdent, r.FromPosition)
+}
+
+// MarshalJSON implements encoding/json.Marshaler interface
+func (r Ref) MarshalJSON() ([]byte, error) {
+	type pkg struct {
+		Importpath string `json:"import_path"`
+		Version    int64  `json:"version"`
+	}
+	type location struct {
+		Position Position `json:"position"`
+		Pkg      pkg      `json:"package"`
+		Ident    string   `json:"ident"`
+	}
+	type refForJSON struct {
+		From location `json:"from"`
+		To   location `json:"to"`
+		Typ  RefType  `json:"type"`
+	}
+	return json.Marshal(refForJSON{
+		From: location{
+			Position: r.FromPosition,
+			Pkg: pkg{
+				Importpath: r.FromPackage.Path,
+				Version:    r.FromPackage.Version,
+			},
+			Ident: r.FromIdent,
+		},
+		To: location{
+			Position: r.ToPosition,
+			Pkg: pkg{
+				Importpath: r.ToPackage.Path,
+				Version:    r.FromPackage.Version,
+			},
+			Ident: r.ToIdent,
+		},
+		Typ: r.RefType,
+	})
 }
